de3513dd35eb9c4757d9f9f95f526a4a320e13de
Costa Paraskevopoulos <costa.paras1@gmail.com>
2021-04-02 14:00:32 +1100

    Add NewsMonitor class
    Stores news url and information about data to extract/monitor
    Compares, stores/updates and notifies of updates to page (tracked items)

--- src/main.py
+++ src/main.py
@@ -5,31 +5,64 @@ from scraper import ItemScraper
 import requests
 
 
-db_uri = 'sqlite:///news.db'
-url = 'https://www.sbs.com.au/news/'
+DB_URI = 'sqlite:///news.db'
+
+
+class NewsMonitor:
+
+    def __init__(self, db):
+        self.db = db
+        self.url = 'https://www.sbs.com.au/news/'
+        self.items = [
+            {
+                'item_selector': 'page__section page__section--style-1',
+                'metadata_selectors': [
+                    {'name': 'title', 'class': 'block__title'}
+                ],
+            },
+            {
+                'item_selector': 'preview',
+                'metadata_selectors': [
+                    {'name': 'topic', 'class': 'topic__string'},
+                    {'name': 'title', 'class': 'preview__headline'},
+                    {'name': 'date', 'class': 'date__string'},
+                    {'name': 'url', 'class': 'preview__headline',
+                     'tag': 'a', 'attr': 'href'}
+                ]
+            },
+            {
+                'item_selector': 'menu__list-item',
+                'metadata_selectors': [
+                    {'name': 'topic', 'tag': 'a'},
+                    {'name': 'url', 'tag': 'a', 'attr': 'href'}
+                ]
+            }
+        ]
+
+    def update(self):
+
+        page = requests.get(self.url).content
+        soup = BeautifulSoup(page, 'html.parser')
+
+        item_selector = self.items[0]['item_selector']
+        metadata_selectors = self.items[0]['metadata_selectors']
+
+        section_scraper = ItemScraper(item_selector, metadata_selectors)
+        section_items = section_scraper.scrape(soup)
+        print(section_items)
+        for item in section_items:
+            item['present'] = True
+            item['item_type'] = 'section'
+            self.db.insert_item(item)
+        for item in self.db.get_items('section'):
+            print(self.db.tuple_to_dict(item))
 
 
 def main():
     """Program entrypoint."""
-    db = DBInterface(db_uri)
-
-    page = requests.get(url).content
-    soup = BeautifulSoup(page, 'html.parser')
-
-    item_selector = 'page__section page__section--style-1'
-    metadata_selectors = [
-        {'name': 'title', 'class': 'block__title'}
-    ]
-
-    section_scraper = ItemScraper(item_selector, metadata_selectors)
-    section_items = section_scraper.scrape(soup)
-    print(section_items)
-    for item in section_items:
-        item['present'] = True
-        item['item_type'] = 'section'
-        db.insert_item(item)
-    for item in db.get_items('section'):
-        print(db.tuple_to_dict(item))
+    db = DBInterface(DB_URI)
+    monitor = NewsMonitor(db)
+    monitor.update()
     db.close()
 
 
